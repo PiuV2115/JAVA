1553ffe495200c7fd26ee707b54d7f0ae7bb0479
Priya Kshirsagar <144653180+PiuV2115@users.noreply.github.com>
2025-04-02 08:59:59 +0530

    Update Assi3_Interface.cpp

--- Assignments/Assi3_Interface.cpp
+++ Assignments/Assi3_Interface.cpp
@@ -1,161 +1,71 @@
-import java.util.Scanner;
+// First Interface
+interface A {
+    void methodA();
+}
 
-// Base Class
-class Hospital {
-    String name;
-    int id;
-    String phone;
+// Second Interface
+interface B {
+    void methodB();
+}
 
-    Hospital() {
-        name = "";
-        id = 0;
-        phone = "";
+// Interface C extending multiple interfaces (Multiple Inheritance)
+interface C extends A, B {
+    void methodC();
+}
+
+// Abstract class implementing interface C
+abstract class AbstractClass implements C {
+    final int finalVar = 100; // Final variable (constant)
+
+    // Abstract method (must be implemented in subclass)
+    abstract void abstractMethod();
+
+    // Final method (cannot be overridden)
+    final void finalMethod() {
+        System.out.println("This is a final method from AbstractClass.");
     }
 
-    void accept(Scanner sc) {
-        System.out.print("# Name: ");
-        name = sc.nextLine();
-        System.out.print("# ID: ");
-        id = sc.nextInt();
-        sc.nextLine();
-        System.out.print("# Phone: ");
-        phone = sc.nextLine();
-    }
-
-    void display() {
-        System.out.println("# Name: " + name);
-        System.out.println("# ID: " + id);
-        System.out.println("# Phone: " + phone);
+    // Concrete method (can be inherited)
+    void normalMethod() {
+        System.out.println("This is a normal method from AbstractClass.");
     }
 }
 
-// Derived Class: Doctor
-class Doctor extends Hospital {
-    String speci;
-    int room_no;
-
-    Doctor() {
-        speci = "";
-        room_no = 0;
+// Final class (Cannot be inherited further)
+final class FinalClass extends AbstractClass {
+    // Implementing abstract method
+    void abstractMethod() {
+        System.out.println("Abstract method implemented in FinalClass.");
     }
 
-    void accept(Scanner sc) {
-        super.accept(sc);
-        System.out.print("# Specialization: ");
-        speci = sc.nextLine();
-        System.out.print("# Room No.: ");
-        room_no = sc.nextInt();
-        sc.nextLine();
+    // Implementing methods from interfaces
+    public void methodA() {
+        System.out.println("Method A implemented from Interface A.");
     }
 
-    void display() {
-        System.out.println("Here are Doctor Details:");
-        super.display();
-        System.out.println("# Specialization: " + speci);
-        System.out.println("# Room No.: " + room_no);
+    public void methodB() {
+        System.out.println("Method B implemented from Interface B.");
+    }
+
+    public void methodC() {
+        System.out.println("Method C implemented from Interface C.");
     }
 }
 
-// Derived Class: Nurse
-class Nurse extends Hospital {
-    String shift;
-    int ward_no;
-
-    Nurse() {
-        shift = "";
-        ward_no = 0;
-    }
-
-    void accept(Scanner sc) {
-        super.accept(sc);
-        System.out.print("# Shift: ");
-        shift = sc.nextLine();
-        System.out.print("# Ward No.: ");
-        ward_no = sc.nextInt();
-        sc.nextLine();
-    }
-
-    void display() {
-        System.out.println("Here are Nurse Details:");
-        super.display();
-        System.out.println("# Shift: " + shift);
-        System.out.println("# Ward No.: " + ward_no);
-    }
-}
-
-// Update Class
-class Update {
-    static Scanner sc = new Scanner(System.in);
-
-    static void doctorUpdate(Doctor doc) {
-        System.out.print("Enter old ID: ");
-        int id1 = sc.nextInt();
-        sc.nextLine();
-        if (doc.id == id1) {
-            System.out.println("Enter new Doctor Info:");
-            doc.accept(sc);
-        } else {
-            System.out.println("ID not found.");
-        }
-    }
-
-    static void nurseUpdate(Nurse nur) {
-        System.out.print("Enter old ID: ");
-        int id1 = sc.nextInt();
-        sc.nextLine();
-        if (nur.id == id1) {
-            System.out.println("Enter new Nurse Info:");
-            nur.accept(sc);
-        } else {
-            System.out.println("ID not found.");
-        }
-    }
-}
-public class Main { 
+// Main Class
+public class Main {
     public static void main(String[] args) {
-        Scanner obj = new Scanner(System.in);
-        Nurse nur = new Nurse();
-        Doctor doc = new Doctor();
+        FinalClass obj = new FinalClass();
 
-        int choice;
-        do {
-            System.out.println("\nMenu:");
-            System.out.println("1. Accept Doctor Details");
-            System.out.println("2. Display Doctor Details");
-            System.out.println("3. Accept Nurse Details");
-            System.out.println("4. Display Nurse Details");
-            System.out.println("5. Update Doctor Details");
-            System.out.println("6. Update Nurse Details");
-            System.out.print("Enter your choice: ");
-            choice = obj.nextInt();
-            obj.nextLine();
+        obj.methodA();
+        obj.methodB();
+        obj.methodC();
+        obj.abstractMethod();
+        obj.normalMethod();
+        obj.finalMethod();
 
-            switch (choice) {
-                case 1:
-                    doc.accept(obj);
-                    break;
-                case 2:
-                    doc.display();
-                    break;
-                case 3:
-                    nur.accept(obj);
-                    break;
-                case 4:
-                    nur.display();
-                    break;
-                case 5:
-                    Update.doctorUpdate(doc);
-                    break;
-                case 6:
-                    Update.nurseUpdate(nur);
-                    break;
-                default:
-                    System.out.println("Invalid Choice");
-            }
-
-            System.out.print("\nEnter 1 to continue, 0 to exit: ");
-        } while (obj.nextInt() == 1);
-
-        obj.close();
+        System.out.println("Final Variable: " + obj.finalVar);
+        
+        // obj.finalVar = 200; // Error: Cannot assign value to final variable
     }
 }
